341f7cf753f836067c679c798e8b349252d8d865
modk <github@modk.org>
2014-08-18 11:49:26 +0200

    Fixed indentation

--- traceLine2D.m
+++ traceLine2D.m
@@ -31,88 +31,88 @@ curpt = p1;
 sgx = sign( dir( 1));
 sgy = sign( dir( 2));
 if abs( dir( 1)) < EPS
-		sgx = 0;
+	sgx = 0;
 end
 if abs( dir( 2)) < EPS
-		sgy = 0;
+	sgy = 0;
 end
 
 while abs( norm( curpt - p2)) > EPS
 
-		% abort if too many steps
-		if cnt > MAX_PATH_LEN
-			error( 'cnt > %d', MAX_PATH_LEN);
-		end
+	% abort if too many steps
+	if cnt > MAX_PATH_LEN
+		error( 'cnt > %d', MAX_PATH_LEN);
+	end
 
-		cnt = cnt + 1;
-		
-		% check if final voxel boundaries of final pt reached
-		cc = 0;
-		if sgx > 0
-				if floor( curpt( 1) + EPS) == floor( p2( 1))
-						cc = cc + 1;
-				end
-		elseif sgx < 0
-				if ceil( curpt( 1) - EPS) == ceil( p2( 1))
-						cc = cc + 1;
-				end
-		else
-				cc = cc + 1;
+	cnt = cnt + 1;
+	
+	% check if final voxel boundaries of final pt reached
+	cc = 0;
+	if sgx > 0
+		if floor( curpt( 1) + EPS) == floor( p2( 1))
+			cc = cc + 1;
 		end
-		if sgy > 0
-				if floor( curpt( 2) + EPS) == floor( p2( 2))
-						cc = cc + 1;
-				end
-		elseif sgy < 0
-				if ceil( curpt( 2) - EPS) == ceil( p2( 2))
-						cc = cc + 1;
-				end
-		else
-				cc = cc + 1;
+	elseif sgx < 0
+		if ceil( curpt( 1) - EPS) == ceil( p2( 1))
+			cc = cc + 1;
 		end
-		if cc == 2
-				% we are at the final voxel, exit
-				pts( cnt, :) = p2;
-				ds( cnt) = norm( curpt - p2);
-				break;
+	else
+		cc = cc + 1;
+	end
+	if sgy > 0
+		if floor( curpt( 2) + EPS) == floor( p2( 2))
+			cc = cc + 1;
 		end
+	elseif sgy < 0
+		if ceil( curpt( 2) - EPS) == ceil( p2( 2))
+			cc = cc + 1;
+		end
+	else
+		cc = cc + 1;
+	end
+	if cc == 2
+		% we are at the final voxel, exit
+		pts( cnt, :) = p2;
+		ds( cnt) = norm( curpt - p2);
+		break;
+	end
 
-		% distances to next grid intersection along the standard
-		% base vectors
-		a = INFTY;
-		if sgx > 0
-				a = ceil( curpt( 1) + EPS) - curpt( 1);
-		else
-				a = floor( curpt( 1) - EPS) - curpt( 1);
-		end
-		b = INFTY;
-		if sgy > 0
-				b = ceil( curpt( 2) + EPS) - curpt( 2);
-		else
-				b = floor( curpt( 2) - EPS) - curpt( 2);
-		end
-		
-		% calculate axis sections
-		x = b / ta;
-		if abs( dir( 2)) < EPS
-				x = INFTY * sgx;
-		end		 
-		y = ta * a;
-		if abs( dir( 1)) < EPS
-				y = 0;
-		end
+	% distances to next grid intersection along the standard
+	% base vectors
+	a = INFTY;
+	if sgx > 0
+		a = ceil( curpt( 1) + EPS) - curpt( 1);
+	else
+		a = floor( curpt( 1) - EPS) - curpt( 1);
+	end
+	b = INFTY;
+	if sgy > 0
+		b = ceil( curpt( 2) + EPS) - curpt( 2);
+	else
+		b = floor( curpt( 2) - EPS) - curpt( 2);
+	end
+	
+	% calculate axis sections
+	x = b / ta;
+	if abs( dir( 2)) < EPS
+		x = INFTY * sgx;
+	end		 
+	y = ta * a;
+	if abs( dir( 1)) < EPS
+		y = 0;
+	end
 
-		if a < x
-				% step to the right
-				nextpt = curpt + [a y];
-		else
-				% step to the top
-				nextpt = curpt + [x b];
-		end
+	if a < x
+		% step to the right
+		nextpt = curpt + [a y];
+	else
+		% step to the top
+		nextpt = curpt + [x b];
+	end
 
-		pts( cnt, :) = nextpt;
-		ds( cnt) = norm( curpt - nextpt);
-		curpt = nextpt;
+	pts( cnt, :) = nextpt;
+	ds( cnt) = norm( curpt - nextpt);
+	curpt = nextpt;
 end
 
 path = pts( 1 : cnt, :);
--- traceLine3D.m
+++ traceLine3D.m
@@ -46,117 +46,117 @@ sgy = sign( dir( 2));
 sgz = sign( dir( 3));
 
 if abs( dir( 1)) < EPS
-		sgx = 0;
+	sgx = 0;
 end
 if abs( dir( 2)) < EPS
-		sgy = 0;
+	sgy = 0;
 end
 if abs( dir( 3)) < EPS
-		sgz = 0;
+	sgz = 0;
 end
 
 while abs( norm( curpt - p2)) > EPS
-		
-		% abort if too many steps
-		if cnt > MAX_PATH_LEN
-				error( 'cnt > %d', MAX_PATH_LEN);
+	
+	% abort if too many steps
+	if cnt > MAX_PATH_LEN
+		error( 'cnt > %d', MAX_PATH_LEN);
+	end
+	
+	cnt = cnt + 1;
+	
+	% check if final voxel boundaries of final pt reached
+	cc = 0;
+	if sgx > 0
+		if floor( curpt( 1) + EPS) == floor( p2( 1))
+			cc = cc + 1;
 		end
-		
-		cnt = cnt + 1;
-		
-		% check if final voxel boundaries of final pt reached
-		cc = 0;
-		if sgx > 0
-				if floor( curpt( 1) + EPS) == floor( p2( 1))
-						cc = cc + 1;
-				end
-		elseif sgx < 0
-				if ceil( curpt( 1) - EPS) == ceil( p2( 1))
-						cc = cc + 1;
-				end
-		else
-				cc = cc + 1;
+	elseif sgx < 0
+		if ceil( curpt( 1) - EPS) == ceil( p2( 1))
+			cc = cc + 1;
 		end
-		if sgy > 0
-				if floor( curpt( 2) + EPS) == floor( p2( 2))
-						cc = cc + 1;
-				end
-		elseif sgy < 0
-				if ceil( curpt( 2) - EPS) == ceil( p2( 2))
-						cc = cc + 1;
-				end
-		else
-				cc = cc + 1;
+	else
+		cc = cc + 1;
+	end
+	if sgy > 0
+		if floor( curpt( 2) + EPS) == floor( p2( 2))
+			cc = cc + 1;
 		end
-		if sgz > 0
-				if floor( curpt( 3) + EPS) == floor( p2( 3))
-						cc = cc + 1;
-				end
-		elseif sgz < 0
-				if ceil( curpt( 3) - EPS) == ceil( p2( 3))
-						cc = cc + 1;
-				end
-		else
-				cc = cc + 1;
+	elseif sgy < 0
+		if ceil( curpt( 2) - EPS) == ceil( p2( 2))
+			cc = cc + 1;
 		end
-		
-		if cc == 3
-				% we are at the final voxel, exit
-				pts( cnt, :) = p2;
-				ds( cnt) = norm( curpt - p2);
-				break;
+	else
+		cc = cc + 1;
+	end
+	if sgz > 0
+		if floor( curpt( 3) + EPS) == floor( p2( 3))
+			cc = cc + 1;
 		end
+	elseif sgz < 0
+		if ceil( curpt( 3) - EPS) == ceil( p2( 3))
+			cc = cc + 1;
+		end
+	else
+		cc = cc + 1;
+	end
+	
+	if cc == 3
+		% we are at the final voxel, exit
+		pts( cnt, :) = p2;
+		ds( cnt) = norm( curpt - p2);
+		break;
+	end
 
-		% distances to next grid intersection along the standard
-		% base vectors
-		a = INFTY;
-		if sgx > 0
-				a = ceil( curpt( 1) + EPS) - curpt( 1);
-		elseif sgx < 0
-				a = floor( curpt( 1) - EPS) - curpt( 1);
-		end
-		b = INFTY;
-		if sgy > 0
-				b = ceil( curpt( 2) + EPS) - curpt( 2);
-		elseif sgy < 0
-				b = floor( curpt( 2) - EPS) - curpt( 2);
-		end
-		c = INFTY;
-		if sgz > 0
-				c = ceil( curpt( 3) + EPS) - curpt( 3);
-		elseif sgz < 0
-				c = floor( curpt( 3) - EPS) - curpt( 3);
-		end
-		
-		% calculate axis sections
-		y1 = a * ta;
-		z1 = a * tb;
-		 
-		x2 = b / ta;
-		z2 = b / tc;
+	% distances to next grid intersection along the standard
+	% base vectors
+	a = INFTY;
+	if sgx > 0
+		a = ceil( curpt( 1) + EPS) - curpt( 1);
+	elseif sgx < 0
+		a = floor( curpt( 1) - EPS) - curpt( 1);
+	end
+	b = INFTY;
+	if sgy > 0
+		b = ceil( curpt( 2) + EPS) - curpt( 2);
+	elseif sgy < 0
+		b = floor( curpt( 2) - EPS) - curpt( 2);
+	end
+	c = INFTY;
+	if sgz > 0
+		c = ceil( curpt( 3) + EPS) - curpt( 3);
+	elseif sgz < 0
+		c = floor( curpt( 3) - EPS) - curpt( 3);
+	end
+	
+	% calculate axis sections
+	y1 = a * ta;
+	z1 = a * tb;
+	 
+	x2 = b / ta;
+	z2 = b / tc;
 
-		x3 = c / tb;
-		y3 = c * tc;
-					 
-		v = [a y1 z1; x2 b z2; x3 y3 c];
+	x3 = c / tb;
+	y3 = c * tc;
 
-		% look for the intersection with the smallest distance
-		% by sorting
-		[tmp, ix] = sortrows( abs( v));
-		% first element in sorted indices is the nearest intersection
-		ix = ix( 1);
-		
-		if ix == 1
-				nextpt = curpt + [a y1 z1];
-		elseif ix == 2
-				nextpt = curpt + [x2 b z2];
-		else
-				nextpt = curpt + [x3 y3 c];
-		end
-		
-		pts( cnt, :) = nextpt;
-		ds( cnt) = norm( ( curpt - nextpt) .* discretization);
-		curpt = nextpt;
+	v = [a y1 z1; x2 b z2; x3 y3 c];
+
+	% look for the intersection with the smallest distance
+	% by sorting
+	[tmp, ix] = sortrows( abs( v));
+	% first element in sorted indices is the nearest intersection
+	ix = ix( 1);
+	
+	if ix == 1
+		nextpt = curpt + [a y1 z1];
+	elseif ix == 2
+		nextpt = curpt + [x2 b z2];
+	else
+		nextpt = curpt + [x3 y3 c];
+	end
+	
+	pts( cnt, :) = nextpt;
+	ds( cnt) = norm( ( curpt - nextpt) .* discretization);
+	curpt = nextpt;
 end
 
 path = pts( 1 : cnt, :);
